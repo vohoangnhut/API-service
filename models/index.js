62de89adfdc7cd9c1918c22ada31430d70ce7856
Nhut.Vo <nhut.vo@dou.com.vn>
2017-06-27 16:50:21 +0700

    .

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize')
 const path = require('path')
-
-const db_sequelize = new Sequelize('mysql://root:Teo54321@localhost:3306/db_node_api_service');
-console.log(process.env.JAWSDB_URL);
+var CLEARDB_DATABASE_URL =  'mysql://gfarya5gse36uz1e:yaqc2rixws5xorzr@jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wkvrgvqvsd6157n2?reconnect=true'
+var local_DB_URL = 'mysql://root:Teo54321@localhost:3306/db_node_api_service'
+const db_sequelize = new Sequelize(CLEARDB_DATABASE_URL);
 
 //CLEARDB_DATABASE_URL=mysql://bc49495ded2948:d61c3548@us-cdbr-iron-east-03.cleardb.net/heroku_635a910a6589203?reconnect=true
 //DEV_DATABASE_URL=mysql://root:Teo54321@localhost:3306/db_node
